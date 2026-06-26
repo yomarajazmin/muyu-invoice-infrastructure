d883e6c1ffc8716b2efa0326841a1c5063101ae1
yomarajazmin <yomarajazmin@gmail.com>
2026-06-25 21:17:36 -0400

    parameter fix

--- vpc.tf
+++ vpc.tf
@@ -53,8 +53,8 @@ resource "aws_route_table" "rt_public" {
   vpc_id = aws_vpc.muyu.id
 
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_internet_gateway.igw.id
   }
 #   route {
 #     cidr_block           = "::/0"
@@ -78,8 +78,8 @@ resource "aws_route_table" "rt_private" {
   vpc_id = aws_vpc.muyu.id
 
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.natgw.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.natgw.id
   }
 #   route {
 #     cidr_block           = "::/0"
